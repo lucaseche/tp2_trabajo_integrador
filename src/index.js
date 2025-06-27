1dc2301873fadf098657d70b47099f9e2be38321
JoaquinRouge <Joarouge@gmail.com>
2025-06-26 21:52:01 -0300

    correccion Router e index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import express from "express"
-import dotenv from 'dotenv'
-import Router from "./routes/movies.route.js"
+// src/index.js
+import express from "express";
+import dotenv from 'dotenv';
+import Router from "./routes/movies.route.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", new Router().start())
+const movieRoutes = new Router().start();
+app.use("/", movieRoutes);
 
 app.use((req, res) => {
     res.status(404).json({
         code: 404,
         message: "Recurso no encontrado."
-    })
-})
+    });
+});
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- src/routes/movies.route.js
+++ src/routes/movies.route.js
@@ -1,22 +1,23 @@
+// src/routes/movies.route.js
 import PeliculaController from "../controllers/movies.controller.js";
-import express from 'express'
+import express from 'express';
 import authMidd from "../middleware/auth.js";
 
-class Router{
+class Router {
     constructor() {
-        this.controller = new PeliculaController()
-        this.router = express.Router()
+        this.controller = new PeliculaController();
+        this.router = express.Router();
     }
 
     start() {
-        this.router.post("/login", this.controller.login)
-        this.router.get("/peliculas", authMidd.verifyToken, this.controller.getPeliculas)
-        this.router.post("/peliculas", this.controller.postPelicula)
-        this.router.put("/peliculas/update/all/:id", this.controller.putPelicula)
-        this.router.patch("/peliculas/update/:id", this.controller.patchPelicula)
-        this.router.delete("/delete/:id", this.controller.deletePelicula)
+        this.router.post("/login", this.controller.login);
+        this.router.get("/peliculas", authMidd.verifyToken, this.controller.getPeliculas);
+        this.router.post("/peliculas", this.controller.postPelicula);
+        this.router.put("/peliculas/update/all/:id", this.controller.putPelicula);
+        this.router.patch("/peliculas/update/:id", this.controller.patchPelicula);
+        this.router.delete("/delete/:id", this.controller.deletePelicula);
+        return this.router;
     }
-
 }
 
-export default Router
+export default Router;
