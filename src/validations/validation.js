9eace82efb14ea92b32b99d8d8ddaf1edf2be350
sergicarbajal <sergiocarbajal1991@gmail.com>
2025-06-29 16:22:16 -0300

    Update validation.js

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -4,16 +4,14 @@ const peliculaSchema = Joi.object({
   _id: Joi.string().optional(),
   title: Joi.string().min(1).max(100).required(),
   year: Joi.number().integer().min(1800).max(new Date().getFullYear()).required(),
-  runningTime: Joi.string().pattern(/^\d+h\s\d+m$/).required(), // requiere formato "2h 7m"
-  description: Joi.string().min(10).max(1000).required(),
-  genre: Joi.array().items(Joi.string().valid("Adventure", "Action", "Sci-Fi", "Drama", "Comedy", "Horror",
-        "Romance")).min(1).required()
+  genre: Joi.array().items(Joi.string().required())
 });
 
 const UserSchema = Joi.object({
   _id: Joi.string().optional(),
   username: Joi.string().required(),
-  password: Joi.string().required()
+  password: Joi.string().required(),
+  mail:     Joi.string().required()
 });
 
 export default {
