8c10a90bb9b941963f10da1575e8f94cbcdbb629
sergicarbajal <sergiocarbajal1991@gmail.com>
2025-06-29 22:47:56 -0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,37 +1,36 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
-const SECRETKEY = process.env.SECRET_KEY;
-
+const SECRETKEY = process.env.SECRET_KEY
+console.log("La clave secreta es",SECRETKEY)
+//Generación del token con una firma
 const generateToken = async (data) => {
-    const payload = {
-        username: data.username
-    };
-    const token = jwt.sign(payload, SECRETKEY, { expiresIn: "3h" });
-    return token;
-};
+    //payload -> cualquier proceso que contenga información delicada
+    //Bcrypt
+  
+const payload = {
+    userName: data.userName,
+    password: data.password
+}
 
+    //objeto con info del usuario + mi clave de app + si expira, en cuanto tiempo
+    const tkn = await jwt.sign(payload, SECRETKEY, { expiresIn: '15m'})
+    return tkn
+}
 
+//Verificación del token y de la duración
+//un middleware se situa entre la solicitud y la respuesta
+//next es la palabra reservada para que continue el flujo
 const verifyToken = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization;
+    //generalmente los tokens se envian en los encabezados de cada solicitud
+    const tkn = req.headers.authorization
+    const decoded = await jwt.verify(tkn, SECRETKEY)
+    if(!decoded) res.status(401).json({message: "Token inválido !!!."})
 
-        if (!token) {
-            return res.status(401).json({ message: "Token no proporcionado." });
-        }
-
-        const decoded = jwt.verify(token, SECRETKEY);
-        
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Token inválido o expirado." });
-    }
-};
-
-export { generateToken, verifyToken };
-
+    next()
+}
 
 export default {
     generateToken,
