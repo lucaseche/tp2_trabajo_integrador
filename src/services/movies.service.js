4e6c589bf5d9fa10ffb71781e6aadf6670f56b38
sergicarbajal <sergiocarbajal1991@gmail.com>
2025-07-01 21:12:51 -0300

    Update movies.service.js
    getPeliculasNacionales agregadas

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -4,11 +4,34 @@ import PeliculasModel from "../models/DAO/movies.model.js";
 class PeliculaService{
     constructor() {
         this.model = new PeliculasModel()
+        this.apiKey = process.env.API_KEY || '7c1b527f58e847a486ea442f6d15adec';
     }
 
     getPeliculas = async () => {
         return await this.model.getPeliculas()
     }
+
+    getPeliculasNacionales = async () => {
+  try {
+    // *** No logre usar el API_KEY desde el .env, por lo que lo puse directamente en la URL
+    const geoResponse = await fetch('https://api.ipgeolocation.io/ipgeo?apiKey=7c1b527f58e847a486ea442f6d15adec');   
+    const geoData = await geoResponse.json();
+
+    const { country_name: paisUsuario } = geoData;
+
+    console.log("País del usuario:", paisUsuario);
+
+ 
+    const peliculasDelPais = await this.model.getPeliculasPorPais(paisUsuario);
+
+    return peliculasDelPais;
+
+  } catch (error) {
+    console.error('Error al obtener películas nacionales:', error);
+    return [];
+  }
+};
+
 
     postPelicula = async (pelicula) => {
         const validatePelicula = validation.peliculaSchema.validate(pelicula)
